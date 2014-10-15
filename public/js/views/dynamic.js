f600caf4222e3c7c334c25f5172b6952d9127d0e
Tully Robinson <tully@tojr.org>
2014-10-16 02:00:56 +1100

    Add ExtractModal view

--- public/js/views/dynamic.js
+++ public/js/views/dynamic.js
@@ -22,6 +22,57 @@ var ClickMenu = View.extend({
    }
 });
 
-module.exports = {
+var CollectionSelect = View.extend({
+   el: $('<select name="category" class="collection-list">'),
 
+   init: function() {
+      //this.el.empty();
+   },
+
+   render: function() {
+      this.el.html('');
+      var that = this;
+      var selected = $('.category-title.selected').text() || '';
+      $('.category-title').each(function(key, name) {
+         var option = $('<option>');
+         option.text($(name).text());
+         option.val($(name).text().toLowerCase());
+         if (option.text().toLowerCase() === selected.toLowerCase()) {
+            option.attr('selected', 'selected');
+         }
+         that.el.append(option);
+      });
+      return this;
+   }
+});
+
+module.exports = {
+   ExtractModal: Modal.extend({
+      init: function() {
+         this.el.attr('enctype', 'multipart/form-data');
+
+         this.extractEvents();
+         this.render();
+      },
+
+      extractEvents: function() {
+         var that = this;
+         this.submit.click(function(e) {
+            e.preventDefault();
+            // model logic
+         });
+      },
+
+      render: function() {
+         var select = new CollectionSelect;
+         this.submit.val('Extract');
+
+         this.el.append('<input id="upload-input" type="file" name="links">')
+                .append(select.render().el)
+                .append(this.submit)
+                .append(this.close);
+
+         $('body').append(this.cover).append(this.el);
+      }
+   })
 };
--- public/js/views/static.js
+++ public/js/views/static.js
@@ -6,12 +6,28 @@ var dynamic = require('./dynamic.js');
 var ToolsView = View.extend({
    el: '#player-tools',
 
+   init: function() {
+      this.addMenu = new AddMenuView;
+   },
+
    events: {
       'click #add-button': 'showAddMenu'
    },
 
    showAddMenu: function(e) {
       $('#add-menu', this.el).show();
+   }
+});
+
+var AddMenuView = View.extend({
+   el: '#add-menu',
+
+   events: {
+      'click .extract': 'extractModal'
+   },
+
+   extractModal: function() {
+      var extract = new dynamic.ExtractModal;
    }
 });
 
